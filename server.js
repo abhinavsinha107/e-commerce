180483973091886de121ae20b3e9dff10c384697
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-07-24 18:01:10 +0530

    sixth commit

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//   });
+// }
 
 app.get("/", (req, res) => {
     res.send("Welcome to ecommerce website.")
